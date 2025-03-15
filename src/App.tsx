700ce914216d0d4ac2af5f0e22d5c08b081d209f
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-15 17:43:19 +0200

    input component init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 // components
 import Header from './components/Header';
+import Input from './components/Input';
 
 function App() {
   return (
     <main className='w-screen h-screen bg-blue-800'>
       <div className='max-w-screen-xl m-auto p-16'>
         <Header />
+        <section>
+          <Input />
+        </section>
       </div>
     </main>
   );
